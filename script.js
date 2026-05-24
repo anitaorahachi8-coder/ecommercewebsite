c75557062077659035c738465c3532fec39e325b
anitaorahachi8-coder <anitaorahachi8@gmail.com>
2026-05-24 19:53:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function addtocart(name, price, qty){
-  let name= 
+  let name = document.getElementById(p1).innerText;
+  let price = parseInt( document.getElementById(price1).innerText;
+  let qty = parseInt( document.getElementById(qtyId).value );
+  let total = price*qty
+  let store = localStorage.getItem("cart");
 }
